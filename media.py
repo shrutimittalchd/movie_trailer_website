95c2291f2e4903fcd7f74d5ca8ce59adf279ef68
Shruti Mittal <33623468+shrutimittalchd@users.noreply.github.com>
2017-12-16 13:53:12 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,8 +1,9 @@
-import webbrowser # it will be later used to open the url of trailer
+import webbrowser  # it will be later used to open the url of trailer
+
 
 # this class Movie will take the arguments in __init__
 class Movie():
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -10,4 +11,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
